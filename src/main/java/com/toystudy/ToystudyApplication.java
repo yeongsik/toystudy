bb0e07b40c393f3be0ee3d881e51162a9e442208
kwon-yeongsik <youngsik0266@gmail.com>
2022-11-29 12:26:49 +0900

    first setting

---
+++ src/main/java/com/toystudy/ToystudyApplication.java
@@ -0,0 +1,13 @@
+package com.toystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToystudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToystudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toystudy/ToystudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToystudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
